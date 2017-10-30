1500f46b422ae3759c9791b5a26e7f1d86defb75
Jonathan Schmidt <jonathan.schmidt@autodesk.com>
2017-10-30 13:43:22 -0400

    First commit

---
+++ Assets/Editor/Database.cs
@@ -0,0 +1,28 @@
+﻿using SyntaxTree.VisualStudio.Unity.Bridge;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class UnityVSFixEditorResourcesScriptPaths
+{
+    static UnityVSFixEditorResourcesScriptPaths()
+    {
+        ProjectFilesGenerator.ProjectFileGeneration += (name, content) =>
+        {
+            content = content.Replace("UnityPackageManager\\com.unity.editor.resources", "C:\\work\\unity\\External\\PackageManager\\Editor\\com.unity.editor.resources\\package");
+            return content;
+        };
+    }
+}
+
+[InitializeOnLoad]
+public class UnityVSProjectGenerationFix
+{
+    static UnityVSProjectGenerationFix()
+    {
+        ProjectFilesGenerator.ProjectFileGeneration += (name, content) =>
+        {
+            content = content.Replace("</AssemblyName>", EditorSettings.Internal_UserGeneratedProjectSuffix + "</AssemblyName>");
+            return content;
+        };
+    }
+}
---
+++ Assets/Editor/Tests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class Tests
+{
+    [MenuItem("ERDP/Package Access Tests")]
+    public static void PackageAccessTests()
+    {
+        var assetPath = "Assets/Utilities/Editor/Converter.cs";
+        var asset = AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(assetPath);
+        Debug.Log(String.Format("File.Exists({0}) returns {1}", assetPath, File.Exists(assetPath)));
+        Debug.Log(String.Format("Asset object {0} exists at {1}", assetPath, AssetDatabase.GetAssetPath(asset)));
+
+        assetPath = AssetDatabase.GetAssetPath(asset); //"UnityPackageManager/com.unity.editor.resources/Assets/Utilities/Editor/Converter.cs";
+        asset = AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(assetPath);
+        Debug.Log(String.Format("File.Exists({0}) returns {1}", assetPath, File.Exists(assetPath)));
+        Debug.Log(String.Format("Asset object {0} named {1}", assetPath, asset.name));
+
+        assetPath = "Assets/Editor/Tests.cs";
+        Debug.Log(String.Format("File.Exists({0}) returns {1}", assetPath, File.Exists(assetPath)));
+        
+        var assetPaths = AssetDatabase.GetAllAssetPaths().Where(p => p.EndsWith(".cs"));
+        Debug.Log(String.Format("Assets {0}", String.Join(", ", assetPaths.ToArray())));
+
+        //TODO: AssetDatabase.GetPackageRootPath("com.unity.editor.resources");
+        //TODO: AssetDatabase.GetPackageAssetPath("UnityPackageManager/com.unity.editor.resources/Assets/Utilities/Editor/Converter.cs");
+    }
+
+    [MenuItem("ERDP/Get Icon Tests")]
+    public static void GetIcon()
+    {
+        var iconTexture = EditorGUIUtility.FindTexture("UxmlScript Icon");
+        Debug.Log(String.Format("Icon {0} exists at {1}", iconTexture.name, AssetDatabase.GetAssetPath(iconTexture)));
+
+        iconTexture = EditorGUIUtility.FindTexture("UssScript Icon");
+        Debug.Log(String.Format("Icon {0} exists at {1}", iconTexture.name, AssetDatabase.GetAssetPath(iconTexture)));
+
+        iconTexture = EditorGUIUtility.FindTexture("StyleSheet Icon");
+        Debug.Log(String.Format("Icon {0} exists at {1}", iconTexture.name, AssetDatabase.GetAssetPath(iconTexture)));
+    }
+}
